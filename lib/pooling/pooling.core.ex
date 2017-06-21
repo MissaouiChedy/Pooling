a79b2618164ff3deeca6ffcc84d9ac3d05b38d65
Chedy Missaoui <missaoui.chedy@gmail.com>
2017-06-21 18:04:23 +0100

    created a prime function

--- lib/pooling/pooling.core.ex
+++ lib/pooling/pooling.core.ex
@@ -3,5 +3,29 @@ defmodule Pooling.Core do
     Enum.zip(0..count, 0..count)
       |> Enum.map(fn({l, r}) -> l*r end)
       |> Enum.reduce(0, fn(elem, acc) -> elem + acc end)
-    end
+  end
+
+  def primes n do
+    Enum.filter(1..(n-1), fn num -> is_prime?(num) end)
+  end
+
+  def is_prime? 1 do
+    true
+  end
+
+  def is_prime?(n) when (n > 1) do
+    is_prime?(n, n-1)
+  end
+
+  defp is_prime? _n, 1 do
+    true
+  end
+
+  defp is_prime?(n, d) when rem(n,d) == 0 do
+    false
+  end
+  
+  defp is_prime?(n, d) do
+    is_prime?(n, d-1)
+  end
 end
